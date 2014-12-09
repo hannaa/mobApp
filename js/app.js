fceb8dbbe5496db87b5211af70240156e706850b
Hanna-Kaisa Alanne <hanna-kaisa.alanne@metropolia.fi>
2014-12-09 13:37:12 +0200

    css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,83 @@
 $(document).ready(function(){
-	//ensimmäinen versio: löydä parit! :D
+
+/****************************** aloitusruutu ************************************************/
+
+	// piilottaa pelielementit sisältävän divin, kun sivu avautuu:
+	$('#game_screen').hide(); 				
 	
+	// "PLAY!" -painiketta painaessa 
+	$('#play_button').click(
+		function(){ 
+			// piilottaa aloitusruutu-divin ja näyttää pelielementit sisältävän divin
+			$('#start_screen').hide();
+			$('#game_screen').show();
+			
+			// asettaa laskurin sekunnit ja minuutit nollaksi
+			s_time = 0;								
+			m_time = 0;	
+			
+			// asettaa parilaskurin nollaksi ja poistaa parit pistekasasta
+			pairs = 0;
+			tries = 0;
+			$('#points').empty();
+
+			// sekoittaa kortin kuvat (funktio shuffle() löytyy alempaa
+			shuffle(color);	
+
+			// asettaa sekoitetut kortit pelipöytään
+			for(i=0 ; i <= 15; i ++){
+				$('#kortti_' + (i+1)).attr('src','kortit/kortti_' + color[i] +'.png');
+				$('#kortti_takaa_' + (i+1)).attr('src','kortit/kortti_takaa.png');
+			};
+			
+			//piilottaa korttien etupuolen kuvat sisältävät divit ja näyttää kuvat takaa
+			for(var i=1;i<=16;i++){
+				$('#kortti_' + i).hide();
+				$('#kortti_takaa_' + i).show();
+			}
+		}
+	);
+
+/*************************** funktioita ja muuttujia ***************************************/
+
+	// määritellään muuttujat: 
+	
+		//ajastimen sekunneille ja minuuteille
+	var s_time;								
+	var m_time;	
+	
+		// pelilogiikan muuttujat
+	var pairs;
+	var tries;
+	var open_cards = 0;
+	var open_1;
+	var open_2;
+	var closed_1;
+	var closed_2;
+	var color_1;
+	var color_2;
+	
+		// lisätään kortit (2x jokaista väriä) muuttujaan 'color'
+	var color = [	
+		'keltainen',
+		'harmaa',
+		'lila',
+		'pink',
+		'punainen',
+		'sininen',
+		'tsininen',
+		'vihrea',
+		'keltainen',
+		'harmaa',
+		'lila',
+		'pink',
+		'punainen',
+		'sininen',
+		'tsininen',
+		'vihrea'
+	];
+
+	// korttien sekoittamiseen lainattu Fisher-Yates Shuffle -funktio
 	function shuffle(array) {
 		var l = array.length;
 		while(l) {
@@ -12,189 +89,245 @@ $(document).ready(function(){
 		console.log(array);
 		return array;
 	}
-	var random = [	'#kortti_1', 
-					'#kortti_2', 
-					'#kortti_3', 
-					'#kortti_4', 
-					'#kortti_5', 
-					'#kortti_6', 
-					'#kortti_7', 
-					'#kortti_8',
-					'#kortti_9', 
-					'#kortti_10', 
-					'#kortti_11', 
-					'#kortti_12', 
-					'#kortti_13', 
-					'#kortti_14', 
-					'#kortti_15', 
-					'#kortti_16'];
-					
-	var color = [	'keltainen',
-					'harmaa',
-					'lila',
-					'pink',
-					'punainen',
-					'sininen',
-					'tsininen',
-					'vihrea',
-					'keltainen',
-					'harmaa',
-					'lila',
-					'pink',
-					'punainen',
-					'sininen',
-					'tsininen',
-					'vihrea'];
 	
+	// määritellään ajastin kutsumaan funktiota sekunnin välein
+	var timer = setInterval(				
+		function(){		
+			// joka sekunti s_time kasvaa yhdellä
+			s_time ++;	
+			
+			// jos molemmat aikamuuttujat alle 10, lisää kummankin eteen nollan ja kirjoittaa ajastimen paikalle
+			if(s_time < 10 && m_time < 10){						 
+				$('#timer').text("0" + m_time + ":0" + s_time);
+				
+			// jos s_time yli 10 ja alle 60, lisää vain minuuteille nollan eteen, kirjoittaessaan arvot ajastimeen
+			}else if(s_time < 60 && m_time < 10){				
+				$('#timer').text("0" + m_time + ":" + s_time);
+				
+			// kun sekunnit ylittävät 60, 
+			}else{
+				// minuuttilaskuri kasvaa yhdellä ja sekunttilaskuri nollataan
+				m_time ++;					
+				s_time = 0;
+				
+				// minuuttilaskurin saavuttaessa 10 (!lukua muuttamalla peliaika lyhenee)
+				if(m_time > 9){		
+					// laskuri tyhjennetään, peli katoaa näkyvistä, ajastimen tilalle "Game over!"
+					clearInterval(timer);
+					$('.yksi').hide();
+					$('.kaksi').hide();
+					$('.kolme').hide();
+					$('.nelja').hide();
+					$('#timer').text("Game over!");
+				}
+			}
+		}
+	,
+	1000);
 	
-	// Fisher-Yates - "shuffle" funktiota kutsutaan "kysymykset"-arrayhin. Ja näin pakka on sekaisin!
-	$('#play_button').click(
-		function(){ 
-		shuffle(random);
-		shuffle(color);
-		for(i=0 ; i <= 15; i ++){
-			$(random[i]).attr('src','kortit/kortti_' + color[i] +'.png');
-		};
-		 
-	}); 
-	shuffle(random);
-	
-	
-	for(i=0 ; i <= 15; i ++){
-		$(random[i]).hide();
-		$(random[i]).attr('src','kortit/kortti_' + color[i] +'.png');
-	};
-	
-	$('#kortti_takaa_1').click(function(){
-		$('#kortti_takaa_1').hide();
-		$('#kortti_1').show();
-	});
-	$('#kortti_takaa_2').click(function(){
-		$('#kortti_takaa_2').hide();
-		$('#kortti_2').show();
-	});
-	$('#kortti_takaa_3').click(function(){
-		$('#kortti_takaa_3').hide();
-		$('#kortti_3').show();
-	});
-	$('#kortti_takaa_4').click(function(){
-		$('#kortti_takaa_4').hide();
-		$('#kortti_4').show();
-	});
-	$('#kortti_takaa_5').click(function(){
-		$('#kortti_takaa_5').hide();
-		$('#kortti_5').show();
-	});
-	$('#kortti_takaa_6').click(function(){
-		$('#kortti_takaa_6').hide();
-		$('#kortti_6').show();
-	});
-	$('#kortti_takaa_7').click(function(){
-		$('#kortti_takaa_7').hide();
-		$('#kortti_7').show();
-	});
-	$('#kortti_takaa_8').click(function(){
-		$('#kortti_takaa_8').hide();
-		$('#kortti_8').show();
-	});
-	$('#kortti_takaa_9').click(function(){
-		$('#kortti_takaa_9').hide();
-		$('#kortti_9').show();
-	});
-	$('#kortti_takaa_10').click(function(){
-		$('#kortti_takaa_10').hide();
-		$('#kortti_10').show();
-	});
-	$('#kortti_takaa_11').click(function(){
-		$('#kortti_takaa_11').hide();
-		$('#kortti_11').show();
-	});
-	$('#kortti_takaa_12').click(function(){
-		$('#kortti_takaa_12').hide();
-		$('#kortti_12').show();
-	});
-	$('#kortti_takaa_13').click(function(){
-		$('#kortti_takaa_13').hide();
-		$('#kortti_13').show();
-	});
-	$('#kortti_takaa_14').click(function(){
-		$('#kortti_takaa_14').hide();
-		$('#kortti_14').show();
-	});
-	$('#kortti_takaa_15').click(function(){
-		$('#kortti_takaa_15').hide();
-		$('#kortti_15').show();
-	});
-	$('#kortti_takaa_16').click(function(){
-		$('#kortti_takaa_16').hide();
-		$('#kortti_16').show();
-	});
-	
-	$('#kortti_1').click(function(){
-		$('#kortti_1').hide();
-		$('#kortti_takaa_1').show();
-	});
-	$('#kortti_2').click(function(){
-		$('#kortti_2').hide();
-		$('#kortti_takaa_2').show();
-	});
-	$('#kortti_3').click(function(){
-		$('#kortti_3').hide();
-		$('#kortti_takaa_3').show();
-	});
-	$('#kortti_4').click(function(){
-		$('#kortti_4').hide();
-		$('#kortti_takaa_4').show();
-	});
-	$('#kortti_5').click(function(){
-		$('#kortti_5').hide();
-		$('#kortti_takaa_5').show();
-	});
-	$('#kortti_6').click(function(){
-		$('#kortti_6').hide();
-		$('#kortti_takaa_6').show();
-	});
-	$('#kortti_7').click(function(){
-		$('#kortti_7').hide();
-		$('#kortti_takaa_7').show();
-	});
-	$('#kortti_8').click(function(){
-		$('#kortti_8').hide();
-		$('#kortti_takaa_8').show();
-	});
-	$('#kortti_9').click(function(){
-		$('#kortti_9').hide();
-		$('#kortti_takaa_9').show();
-	});
-	$('#kortti_10').click(function(){
-		$('#kortti_10').hide();
-		$('#kortti_takaa_10').show();
-	});
-	$('#kortti_11').click(function(){
-		$('#kortti_11').hide();
-		$('#kortti_takaa_11').show();
-	});
-	$('#kortti_12').click(function(){
-		$('#kortti_12').hide();
-		$('#kortti_takaa_12').show();
-	});
-	$('#kortti_13').click(function(){
-		$('#kortti_13').hide();
-		$('#kortti_takaa_13').show();
-	});
-	$('#kortti_14').click(function(){
-		$('#kortti_14').hide();
-		$('#kortti_takaa_14').show();
-	});
-	$('#kortti_15').click(function(){
-		$('#kortti_15').hide();
-		$('#kortti_takaa_15').show();
-	});
-	$('#kortti_16').click(function(){
-		$('#kortti_16').hide();
-		$('#kortti_takaa_16').show();
-	});
+	// avonaisten korttien jäljittäjä "opener()" kutsutaan kortin tunnuksella klikattaessa
+	function opener(key){
+		return function(){
+				// kortin tunnus (1-16) tallennetaan jäljittäjän muuttujaan
+				var o = key;
 
+				// tunnusta vastaavan kortti käännetään
+				$('#kortti_takaa_' + o).hide();
+				$('#kortti_' + o).show();
+				
+				// käännetty kortti lisätään laskuriin
+				open_cards++;
+
+				// ensimmäisen avatun kortin tiedot tallentuu
+				if(open_cards == 1){
+					open_1 = $('#kortti_' + o);
+					closed_1 = $('#kortti_takaa_' + o);
+					color_1 = color[ (o-1) ];
+				}
+				
+				// toisen avatun kortin tiedot tallentuu
+				if(open_cards == 2){
+					open_2 = $('#kortti_' + o);
+					closed_2 = $('#kortti_takaa_' + o);
+					color_2 = color[ (o-1) ];
+					paired();
+				}
+				
+				//testing:
+				console.log('slot: ' +o);
+				console.log('open cards: ' +open_cards);
+				console.log('pairs: ' + pairs);
+				console.log('c1:' +  color_1);
+				console.log('c2:' + color_2);
+				/*
+				console.log('open_1: ' + open_1);
+				console.log('open_2: ' + open_2);
+				console.log('closed_1: ' + closed_1);
+				console.log('closed_2: ' + closed_2);
+				*/
+				}
+	}
 	
 	
+	// parittaja funktio "paired()" tarkistaa ovatko avatut kortit pari
+	function paired(){
+		tries ++;
+		console.log("Try: " + tries);
+		
+		
+		
+		// jos kortit vastaavat toisiaan 
+		//if(open_1.attr('src') == open_2.attr('src')){
+		if(color_1 == color_2){
+			// parilaskuri kasvaa yhdellä
+			pairs ++;
+			if(pairs==8){
+				win();
+			}
+			// käännettyjen korttien laskuri nollataan
+			open_cards = 0;
+			
+			// kutsutaan kuvannäyttäjä funktio paria vastaavalla värillä
+			showimage(color_1);
+			
+			// kortit näkyvät avoimina 0,7 sekuntia
+			var closer = setTimeout(function(){
+			
+				
+				
+				// neljä ensimmäistä paria kasautuvat ensimmäiseen pinoon
+				if(pairs <= 4){
+					$('#points').html('<img id="pair_kuva" type="button" src="parit/parit_' + pairs + '.png">');
+				
+				// parit 5-8 kasautuvat toiseen pinoon				
+				}else{
+					$('#points').html(
+						'<img id="pair_kuva" type="button" src="parit/parit_4.png">'
+						+
+						'<img id="pair_kuva_2" type="button" src="parit/parit_' + pairs + '.png">'
+					);
+				};
+				
+				// kasoihin siirtyvät kortit poistuvat pelistä
+				open_1.removeAttr('src');
+				open_2.removeAttr('src');
+				closed_1.removeAttr('src');
+				closed_2.removeAttr('src');
+				
+				// tyhjennetään avattujen korttien väritiedot
+				color_1 = undefined;
+				color_2 = undefined;
+			}
+			,
+			1000);
+		
+		// jos kortit eivät vastaa toisiaan kortit kääntyvät takaisin 0,7 sekunnin kuluttua
+		}else{
+			var closer = setTimeout(function(){
+				for(var x = 1; x <= 16; x ++){
+					$('#kortti_' + x).hide();
+					$('#kortti_takaa_' + x).show();
+				}
+				
+				// tyhjennetään avattujen korttien väritiedot
+				color_1 = undefined;
+				color_2 = undefined;
+			}
+			,
+			700);
+		}
+	}
+	
+	// funktio "showimage()" väläyttää ruudulla saadun parin mukaisen kuvan
+	function showimage(color_1){
+	
+		// määrittelee annetun värin kuvannäyttäjän sisäiseen muuttujaan
+		var c = color_1;
+		
+		// lisää peliruutuun paria vastaavan värisen kuvan
+		$('#game_screen').append('<div id="got_pair"><img id="gp_image" src="parit/pari_' + c + '.png"></div>');
+		
+		// antaa käytettävälle diville ja välähtävälle kuvalle css:arvoja
+		$("#got_pair").css({
+			height: '100%',
+			width: '100%',
+			position: 'fixed',
+			top: 0,
+			left: 'auto',
+			right: 'auto',
+			display: 'none'
+			}).show()
+		$("#gp_image").css({
+			width: '90%',
+			left: 'auto',
+			right: 'auto',
+			top: '10%',
+			bottom: '10%',
+			display: 'none'
+			}).show()
+		
+		// poistaa kuvadivin sekunnin kuluttua
+		setTimeout(function() {
+			$("#got_pair").remove();
+			}, 
+		1500)
+	}
+
+/***************************** peliruutu *****************************************************/
+	
+	// "BACK" -painiketta painettaessa
+	$('#back_button').click(
+		function(){
+			// pelin sisältävä divi piiloutuu ja aloitusruutu-divi palaa näkyviin
+			$('#game_screen').hide();
+			$('#start_screen').show();
+
+			// asettaa ajastimen tekstin sekä sekunti- ja minuuttilaskurin nollille
+			$('#timer').text("00:00");
+			s_time = 0;
+			m_time = 0;
+		}
+	);
+	
+	// kortin takakuvaa klikattaessa kutsutaan "opener()", joka vaihtaa takakuvan tilalle etukuvan
+	for(var x = 1; x <= 16; x ++){
+		$('#kortit').on('click', '#kortti_takaa_' + x, opener(x));
+	}
+
+	/*
+	else{
+		for(var x = 1; x <= 16; x ++){
+		$('#kortit').off('click', '#kortti_takaa_' + x, opener(x));
+	}
+	}
+	
+	while(open_cards == 2){
+	for(var x = 1; x <= 16; x ++){
+		$('#kortit').off('click', '#kortti_takaa_' + x, opener(x));
+	}
+	}
+	*/
+	
+	// klikattaessa korttien alueella mitä tahansa, kääntölaskuri on kaksi, se nollaantuu
+	$('#kortit').click(
+		function(){
+			if(open_cards == 2){
+				open_cards = 0;
+			}
+		}
+	);
+	
+/***************************** voitto! *****************************************************/
+
+	function win(){		
+		$('#start_screen').show();
+		$('#game_screen').hide();
+
+		$.ajax({url:"win.txt",success:function(result){
+			$("#win").html(result);
+			$('#tries').append(tries + " tries ");
+			$('#time').append(m_time + " minutes and " + s_time + " seconds");
+		}});
+		
+	}
 });
